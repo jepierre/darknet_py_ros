e5e43a0f0d284ccafda6b08435bbe7596d2d893d
Jean Pierre <jpierre@unm.edu>
2021-01-25 16:41:18 -0700

    update build script

--- build_darknet.sh
+++ build_darknet.sh
@@ -8,6 +8,11 @@ sed -i 's/LIBSO=0/LIBSO=1/g' darknet/Makefile
 ## don't use opencv in Darknet or you'll run into issues with python3 cv2
 # sed -i 's/OPENCV=0/OPENCV=1/g' darknet_py_ros/darknet/Makefile
 
+# set library path
+export PATH="/usr/local/cuda-10.2/bin:$PATH"
+export LD_LIBRARY_PATH="/usr/local/cuda-10.2/lib64/:$LD_LIBRARY_PATH"
+
 # Build Darknet
 cd darknet
 make -j8
+
